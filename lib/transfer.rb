497db31bbe27c67400038fa28af158db132d8d14
Alla Kuzminova <alchik@gmail.com>
2020-11-04 23:26:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,4 +12,10 @@ class Transfer
   def valid?
     self.sender.valid? && self.receiver.valid?
   end
+
+  def execute_transaction
+    sender.balance += amount
+    receiver.balance -= amount
+    self.status = "completed"
+  end
 end
